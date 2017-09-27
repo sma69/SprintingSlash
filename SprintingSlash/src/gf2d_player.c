1ac33951d30f5b35836f75c2e126fcc2e4986801
sma69 <sma69@njit.edu>
2017-09-26 20:32:36 -0400

    player header file created

--- SprintingSlash/src/gf2d_player.c
+++ SprintingSlash/src/gf2d_player.c
@@ -1,8 +1,20 @@
 #include "gf2d_entity.h"
 
-Entity *newPlayer(Vector2D Position, const char *name)
+Entity *player_new(Vector2D Position)
 {
-	Entity *ent;
-	int i;
-	
+	Entity * self;
+	self = entity_new();
+
+	if (!self)
+	{
+		slog("player does not exist\n");
+		system("PAUSE");
+		return NULL;
+	}
+	self->sprite = gf2d_sprite_load_all("images/players/redSquare.png", 32, 32, 1);
+	vector2d_set(self->position, Position.x, Position.y);
+	vector4d_set(self->color, 255, 255, 255, 255);
+	self->frame = 1;
+	slog("player spawned");
+	return self;
 }
